942e462b73bcbcb5d8a4b3d79a547a4297a0d901
eyhdev <alaqsa335@gmail.com>
2023-08-11 14:57:11 +0300

    Initial Commit

---
+++ ActivityApp/ActivityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityAppApp.swift
+//  ActivityApp
+//
+//  Created by eyh.mac on 11.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ActivityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
